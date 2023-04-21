95d1e5a120c972d94bf8acfde8ab56fa736c0104
Fabius Vogel <vogel.fabius@gmail.com>
2023-04-21 10:41:19 +0200

    Added basic Dialogflow API integration

---
+++ classification/dialogflow.py
@@ -0,0 +1,35 @@
+from google.cloud import dialogflow_v2beta1 as dialogflow
+import json
+import requests
+
+
+bearer_token = json.load(open("authentication/gcloud/oauth/oauth.json"))["token"]
+#get key from credentials/gcloud_api.json
+key = json.load(open("authentication/gcloud/client_api_key.json"))["api_key"]
+
+def detect_intent_texts(session_id, texts, lang):
+    #en-US
+    project_id = "llm-assistant-1679252411498"
+
+    url = 'https://dialogflow.googleapis.com/v2/projects/llm-assistant-1679252411498/agent/environments/draft/users/1/sessions/12:detectIntent?key=' + key
+
+    headers = {
+        'Authorization': 'Bearer ' + bearer_token,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        'queryInput': {
+            'text': {
+                'text': 'Wie viel Uhr ist es in Neustadt?',
+                'languageCode': 'de'
+            }
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    
+    print(response.json()["queryResult"]["parameters"])
+
+print(detect_intent_texts("1", "Wie viel Uhr ist es in Neustadt?", "de"))
