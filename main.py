cb92d0291cb356a5ec79f5f1aa5933382c5c920f
Fabius Vogel <vogel.fabius@gmail.com>
2023-07-03 18:48:39 +0200

    Added debugging statements

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ openai.api_key = json.load(open("authentication/openai/openai_key.json"))["api_k
 
 #Initialize chat conversation
 conversation = Conversation()
-conversation.messages = [ChatMessage("system", "The current date is: 15. June. The year is 2023. If you require real time information on a topic that could have changed since your training stoped, fallback to the realtime_websearch function. This can be used multiple times throughout the conversation.")]
+conversation.messages = [ChatMessage("system", "The current date is: 15. June. The year is 2023. Perform a web-search on time sensitive topics that could have changed or you just dont know. Always perform a search, before claiming you dont know the answer.,")]
 
-while True:
+while True: #FIXME: Handle openai.error.RateLimitError 
 	message = input("User: ")
 	conversation.messages.append(ChatMessage("user", message))
 
@@ -24,17 +24,21 @@ while True:
 		arguments = json.loads(response["choices"][0]["message"]["function_call"]["arguments"])
 		name = response["choices"][0]["message"]["function_call"]["name"]
 		function_response = functions.function_dict[name](arguments)
+		print("reponse")
+		print(function_response)
 
-		conversation.messages.append(ChatMessage("user", function_response, True))
+		conversation.messages.append(ChatMessage("user", function_response, True)) #TODO: Change system to user if required
 		
 		response = openai.ChatCompletion.create(
 				model="gpt-3.5-turbo-0613",
 				messages=conversation.convertToOpenAI(),
 				functions= functions.openai_function_documentation,
-				function_call="auto",
+				function_call="none",
 		)
 		
-		conversation.messages.append(ChatMessage("user", response["choices"][0]["message"]["content"]))
+		conversation.messages.append(ChatMessage("user", response["choices"][0]["message"]["content"])) 
+		print(response)
+		print("Response: ")
 		print(response["choices"][0]["message"]["content"])
 	else:
 		conversation.messages.append(ChatMessage("user", response["choices"][0]["message"]["content"]))
