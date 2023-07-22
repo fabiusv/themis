9620049bdef42d56de90432f402267dabb128caf
Fabius Vogel <vogel.fabius@gmail.com>
2023-07-22 16:59:09 +0200

    Changed timestamp format

--- themis/Dataclasses/Context.py
+++ themis/Dataclasses/Context.py
@@ -5,6 +5,7 @@ from .Conversation import Conversation
 from .MetaData import MetaData
 
 class Context(pydantic.BaseModel):
+
    user_id: str
    context_id: str
    creation: datetime.datetime
@@ -15,9 +16,7 @@ class Context(pydantic.BaseModel):
    
 
    def encode(self):
-      return {"user_id": self.user_id, "context_id": self.context_id, "conversation": self.conversation.encode(), "creation": self.creation, "last_modification": self.last_modification, "expiry": self.expiry, "meta_data": self.meta_data.encode()}
+      return {"user_id": self.user_id, "context_id": self.context_id, "creation":int(self.creation.timestamp()), "last_modification": int(self.last_modification.timestamp()), "expiry": int(self.expiry.timestamp()) if self.expiry else None, "conversation": self.conversation.encode(), "meta_data": self.meta_data.encode()}
    @staticmethod
    def decode(context_dict):
-      
-      return Context(user_id= context_dict["user_id"], context_id=context_dict["context_id"], creation=context_dict["creation"], last_modification=context_dict["last_modification"], expiry=context_dict["expiry"], conversation=Conversation.decode(context_dict["conversation"]), meta_data=MetaData.decode(context_dict["meta_data"]))
-
+      return Context(user_id=context_dict["user_id"], context_id=context_dict["context_id"], creation=datetime.datetime.fromtimestamp(context_dict["creation"]), last_modification=datetime.datetime.fromtimestamp(context_dict["last_modification"]), expiry=datetime.datetime.fromtimestamp(context_dict["expiry"]) if context_dict["expiry"] else None, conversation=Conversation.decode(context_dict["conversation"]), meta_data=MetaData.decode(context_dict["meta_data"]))
